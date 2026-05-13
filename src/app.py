7720b3764f2a90a132e95af3c6fa831d9e45e996
Renata Nunes Cotrim <renatanc10@gmail.com>
2026-05-13 15:57:24 -0300

    Integração com API de clima

--- src/app.py
+++ src/app.py
@@ -1,14 +1,41 @@
+import requests
+
 tarefas = []
+
+def obter_clima(cidade):
+    url = f"https://wttr.in/{cidade}?format=j1"
+
+    resposta = requests.get(url)
+
+    if resposta.status_code == 200:
+        dados = resposta.json()
+        temperatura = dados["current_condition"][0]["temp_C"]
+        return temperatura
+
+    return None
+
 
 def adicionar_consumo(quantidade):
     if quantidade <= 0:
         raise ValueError("Quantidade inválida")
     tarefas.append(quantidade)
 
+
 def total_consumido():
     return sum(tarefas)
 
+
 def main():
+
+    cidade = input("Digite sua cidade: ")
+    temperatura = obter_clima(cidade)
+
+    if temperatura:
+        print(f"\nTemperatura atual em {cidade}: {temperatura}°C")
+
+        if int(temperatura) >= 30:
+            print("Está quente! Beba mais água hoje.\n")
+
     while True:
         print("\n1 - Registrar água")
         print("2 - Ver total")
@@ -21,14 +48,16 @@ def main():
                 qtd = float(input("Quantidade em ml: "))
                 adicionar_consumo(qtd)
                 print("Registrado!")
+
             except ValueError:
                 print("Erro ao registrar.")
-        
+
         elif opcao == "2":
             print(f"Total consumido: {total_consumido()} ml")
-        
+
         elif opcao == "3":
             break
+
 
 if __name__ == "__main__":
     main()
